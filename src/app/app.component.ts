3d02a194bf7eb34b90412981284a100c51d3e0d6
Hakan Avci <hkn.avci@hotmail.com>
2021-04-08 22:34:13 +0300

    kamp-frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'northwing';
+  title = 'northwind';
+  user: string = "hakan"
+ 
 }
